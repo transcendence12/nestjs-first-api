c426fffcce1c7c0073ed8031eb313989f3a0e561
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2024-04-10 23:09:33 +0200

    ValidationPipe added to controler to create and update, and in create-dto file added validations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsEnum, IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+    message: 'Valid role required',
+  })
   role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
   ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -29,7 +30,7 @@ export class UsersController {
 
   @Post()
   create(
-    @Body()
+    @Body(ValidationPipe)
     createUserDto: CreateUserDto,
   ) {
     return this.usersService.create(createUserDto);
@@ -38,7 +39,7 @@ export class UsersController {
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body()
+    @Body(ValidationPipe)
     updateUserDto: UpdateUserDto,
   ) {
     return this.usersService.update(id, updateUserDto);
